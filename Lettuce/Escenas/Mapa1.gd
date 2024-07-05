419aecef332116f6a2ee71c0f5d4e3ac54e80c53
arie <luceroariel555@gmail.com>
2024-07-05 11:40:33 -0300

    empiezo modo construccion

---
+++ Lettuce/Escenas/Mapa1.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var map_nodo
+var construir_modo = false
+var construir_valid= false
+var contruir_ubicacion
+var construir_tipo
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _unhandled_input(event):
+	pass
+	
+func iniciar_construc_mode(torre_tipo):
+	pass
+	
