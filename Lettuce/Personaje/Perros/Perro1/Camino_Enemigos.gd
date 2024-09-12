ca8c225334e01f5553b079b8ea4e0d93401af6a4
Quintanta <fuelagor@gmail.com>
2024-09-12 10:44:17 -0300

    Se añadio foto de perros y tomate

--- Lettuce/Personaje/Perros/Perro1/Camino_Enemigos.gd
+++ Lettuce/Personaje/Perros/Perro1/Camino_Enemigos.gd
@@ -1,12 +1,13 @@
 extends Node2D
-@onready var path_follow : PathFollow2D = $Path2D/PathFollow2D
+@onready var path_follow : PathFollow2D = $".."
 @export var speed = 100
-var vida = 10
+var vida = 1000
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	path_follow.progress += delta * speed	
 	_vida()
+	print(vida)
 
 
 func _on_enemigo_1_area_entered(area: Area2D) -> void:
