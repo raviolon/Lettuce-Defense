1e4e99c73fc966e25c9312a6d113cdbe4a4aa70f
Quintana <fuelagor@gmail.com>
2024-11-21 12:33:43 -0300

    daño en las balas

--- Lettuce/Scripts/Stats.gd
+++ Lettuce/Scripts/Stats.gd
@@ -1,11 +1,14 @@
 extends Node2D
 var vida = 10
 
-func _on_enemigo_1_area_entered(area: Area2D) -> void:
-	if area.is_in_group("proyectil"):
-		vida -=1
+# Método para recibir daño de la bala
+func receive_damage(damage: int) -> void:
+	vida -= damage
+	print("Vida del enemigo:", vida)  # Depuración: Verifica que la vida se reduce
+	_vida()
 
+# Función para verificar si el enemigo ha muerto
 func _vida():
 	if vida <= 0:
 		Global.cash += 100
-		queue_free()
+		queue_free()  # Elimina al enemigo
