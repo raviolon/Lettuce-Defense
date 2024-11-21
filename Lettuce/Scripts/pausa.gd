feb796335ffd0765fb179aad361a1f1d027ab7f9
Pato2006 <patrizarate1@gmail.com>
2024-11-21 12:36:25 -0300

    agrego el boton de salir pero con error e index de octa

--- Lettuce/Scripts/pausa.gd
+++ Lettuce/Scripts/pausa.gd
@@ -19,3 +19,12 @@ func _process(delta: float) -> void:
 			color_rect.visible = true
 			get_tree().paused = true  # Pausar el juego
 		presionado = true  # Aseguramos que no se vuelva a alternar hasta la siguiente pulsación
+
+
+func _on_button_pressed() -> void:
+	# Detener procesos específicos de la escena actual si es necesario
+	# Por ejemplo, si tienes un temporizador, lo puedes detener aquí:
+	# $Timer.stop()
+
+	# Cambiar a la nueva escena (por ejemplo, al menú principal)
+	get_tree().change_scene_to_file("res://Escenas/Menu.tscn")
